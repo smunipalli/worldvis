ff15d9ed4189f4484a4471f5aefd9b3c7ae13cbd
smunipalli <smunipalli@ucsd.edu>
2023-01-21 16:11:51 -0600

    Working 3d globe with map

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Globe from "react-globe.gl";
+
+function App() {
+  const N = 300;
+  const gData = [...Array(N).keys()].map(() => ({
+    lat: (Math.random() - 0.5) * 180,
+    lng: (Math.random() - 0.5) * 360,
+    size: Math.random() / 3,
+    color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+  }));
+
+  return (
+    <Globe globeImageUrl="./img/earth-blue-marble.jpg" pointsData={gData} />
+  );
+}
+
+export default App;
