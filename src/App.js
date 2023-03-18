e7f667bc8f0b0342b4f717b320b20d5c7c56906f
smunipalli <smunipalli@ucsd.edu>
2023-03-17 19:10:42 -0500

    Filtering the counties based on the income level data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState, useEffect, useMemo } from "react";
 import * as d3 from "d3";
 import Globe from "react-globe.gl";
 import Button from "@mui/material/Button";
+import _ from "lodash";
 
 function App() {
   const [selectGlobes, setselectGlobes] = useState("regularGlobe");
@@ -100,16 +101,64 @@ function App() {
     );
     colorScale.domain([0, maxVal]);
 
-    console.log(incomeLevel);
+    let match_countries = [];
+    let missing_countries = [];
+
+    let mapping = [
+      { income_level: "North America", geo: "United States of America" },
+    ];
+
+    const filterCountries = () => {
+      _.forEach(incomeLevel, (v) => {
+        const val = _.find(countries.features, (o) => {
+          // return o.properties.ISO_A3 === v.id;
+          return (
+            o.properties.NAME.toLowerCase() === v.value.toLowerCase() ||
+            o.properties.ISO_A3 === v.id
+          );
+        });
+
+        if (val === undefined) {
+          missing_countries.push(v);
+        } else {
+          match_countries.push(val);
+        }
+      });
+
+      // adding missing locations from mappings
+
+      // _.forEach(mapping, (v) => {
+      //   const val = _.find(countries.features, (o) => {
+      //     return o.properties.NAME === v.geo;
+      //   });
+      //   if (val === undefined) {
+      //     missing_countries.push(v);
+      //   } else {
+      //     match_countries.push(val);
+      //   }
+      // });
+
+      // console.log(match_countries);
+      // console.log(missing_countries);
+
+      // console.log(
+      //   _.filter(match_countries, (o) => {
+      //     return o !== undefined;
+      //   })
+      // );
+
+      return match_countries.filter((d) => d.properties.ISO_A2 !== "AQ");
+      // return countries.features.filter((d) => d.properties.ISO_A2 !== "AQ");
+    };
+
+    // console.log(incomeLevel);
 
     return (
       <Globe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
         lineHoverPrecision={0}
-        polygonsData={countries.features.filter(
-          (d) => d.properties.ISO_A2 !== "AQ"
-        )}
+        polygonsData={filterCountries()}
         polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
         polygonCapColor={(d) =>
           d === hoverD ? "steelblue" : colorScale(getVal(d))
