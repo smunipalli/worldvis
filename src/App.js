e07643211d9823b9b84c75ae839793146e376d50
smunipalli <smunipalli@ucsd.edu>
2023-03-19 21:12:12 -0500

    Integrating world population

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useRef } from "react";
 import * as d3 from "d3";
 import Globe from "react-globe.gl";
 import Button from "@mui/material/Button";
@@ -78,6 +78,52 @@ function App() {
       `}
         onPolygonHover={setHoverD}
         polygonsTransitionDuration={300}
+      />
+    );
+  };
+
+  const WorldPopulation = () => {
+    const globeEl = useRef();
+    const [popData, setPopData] = useState([]);
+
+    useEffect(() => {
+      // load data
+      fetch("./geo/world_population.csv")
+        .then((res) => res.text())
+        .then((csv) =>
+          d3.csvParse(csv, ({ lat, lng, pop }) => ({
+            lat: +lat,
+            lng: +lng,
+            pop: +pop,
+          }))
+        )
+        .then(setPopData);
+    }, []);
+
+    useEffect(() => {
+      // Auto-rotate
+      globeEl.current.controls().autoRotate = true;
+      globeEl.current.controls().autoRotateSpeed = 0.1;
+    }, []);
+
+    const weightColor = d3
+      .scaleSequentialSqrt(d3.interpolateYlOrRd)
+      .domain([0, 1e7]);
+
+    return (
+      <Globe
+        ref={globeEl}
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        hexBinPointsData={popData}
+        hexBinPointWeight="pop"
+        hexAltitude={(d) => d.sumWeight * 6e-8}
+        hexBinResolution={4}
+        hexTopColor={(d) => weightColor(d.sumWeight)}
+        hexSideColor={(d) => weightColor(d.sumWeight)}
+        hexBinMerge={true}
+        enablePointerInteraction={false}
       />
     );
   };
@@ -215,6 +261,8 @@ function App() {
         return <ChoroplethGlobe />;
       case "incomeLevel":
         return <IncomeLevelGlobe />;
+      case "worldPopulation":
+        return <WorldPopulation />;
 
       default:
         return <RegularGlobe />;
@@ -305,6 +353,21 @@ function App() {
                     />
                   </ListItemIcon>
                   <ListItemText primary="Income Level" />
+                </ListItemButton>
+                <ListItemButton
+                  onClick={() => {
+                    setselectGlobes("worldPopulation");
+                  }}
+                >
+                  <ListItemIcon>
+                    <PublicIcon
+                      color={(() =>
+                        selectGlobes === "worldPopulation"
+                          ? "primary"
+                          : "inherit")()}
+                    />
+                  </ListItemIcon>
+                  <ListItemText primary="World Population" />
                 </ListItemButton>
               </List>
             </CardActions>
