faadc01d09f462b0fe16db1c0aa242ca495555c9
smunipalli <smunipalli@ucsd.edu>
2023-03-24 00:32:08 -0500

    Cleaning up unused maps

--- src/App.js
+++ src/App.js
@@ -42,53 +42,6 @@ function App() {
 
     return <Globe globeImageUrl="./img/earth-night.jpg" pointsData={gData} />;
   };
-  const ChoroplethGlobe = () => {
-    const [countries, setCountries] = useState({ features: [] });
-    const [hoverD, setHoverD] = useState();
-
-    useEffect(() => {
-      // load data
-      fetch("./geo/ne_110m_admin_0_countries.geojson")
-        .then((res) => res.json())
-        .then(setCountries);
-    }, []);
-
-    const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
-
-    // GDP per capita (avoiding countries with small pop)
-    const getVal = (feat) =>
-      feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
-
-    const maxVal = useMemo(
-      () => Math.max(...countries.features.map(getVal)),
-      [countries]
-    );
-    colorScale.domain([0, maxVal]);
-
-    return (
-      <Globe
-        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-        lineHoverPrecision={0}
-        polygonsData={countries.features.filter(
-          (d) => d.properties.ISO_A2 !== "AQ"
-        )}
-        polygonAltitude={(d) => (d === hoverD ? 0.12 : 0.06)}
-        polygonCapColor={(d) =>
-          d === hoverD ? "steelblue" : colorScale(getVal(d))
-        }
-        polygonSideColor={() => "rgba(0, 100, 0, 0.15)"}
-        polygonStrokeColor={() => "#111"}
-        polygonLabel={({ properties: d }) => `
-        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
-        GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
-        Population: <i>${d.POP_EST}</i>
-      `}
-        onPolygonHover={setHoverD}
-        polygonsTransitionDuration={300}
-      />
-    );
-  };
 
   const WorldPopulation = () => {
     const globeEl = useRef();
@@ -491,8 +444,6 @@ function App() {
     switch (selectGlobes) {
       case "regularGlobe":
         return <RegularGlobe />;
-      case "choroplethGlobe":
-        return <ChoroplethGlobe />;
       case "incomeLevel":
         return <IncomeLevelGlobe />;
       case "energyUse":
@@ -541,44 +492,23 @@ function App() {
                   bgcolor: "background.paper",
                 }}
                 component="nav"
-                // aria-labelledby="nested-list-subheader"
-                // subheader={
-                //   <ListSubheader component="div" id="nested-list-subheader">
-                //     Nested List Items
-                //   </ListSubheader>
-                // }
               >
                 <ListItemButton
                   onClick={() => {
-                    setselectGlobes("regularGlobe");
+                    setselectGlobes("incomeLevel");
                   }}
                 >
                   <ListItemIcon>
                     <PublicIcon
                       color={(() =>
-                        selectGlobes === "regularGlobe"
+                        selectGlobes === "incomeLevel"
                           ? "primary"
                           : "inherit")()}
                     />
                   </ListItemIcon>
-                  <ListItemText primary="Regular Globe" />
+                  <ListItemText primary="Income Level" />
                 </ListItemButton>
 
-                <ListItemButton
-                  onClick={() => {
-                    setselectGlobes("choroplethGlobe");
-                  }}
-                >
-                  <ListItemIcon>
-                    <PublicIcon
-                      color={(() =>
-                        selectGlobes === "choroplethGlobe"
-                          ? "primary"
-                          : "inherit")()}
-                    />
-                  </ListItemIcon>
-                  <ListItemText primary="Choropleth Globe" />
-                </ListItemButton>
                 <ListItemButton
                   onClick={() => {
                     setselectGlobes("energyUse");
@@ -595,18 +525,18 @@ function App() {
 
                 <ListItemButton
                   onClick={() => {
-                    setselectGlobes("incomeLevel");
+                    setselectGlobes("worldPopulation");
                   }}
                 >
                   <ListItemIcon>
                     <PublicIcon
                       color={(() =>
-                        selectGlobes === "incomeLevel"
+                        selectGlobes === "worldPopulation"
                           ? "primary"
                           : "inherit")()}
                     />
                   </ListItemIcon>
-                  <ListItemText primary="Income Level" />
+                  <ListItemText primary="World Population (Cities)" />
                 </ListItemButton>
 
                 <ListItemButton
@@ -637,22 +567,6 @@ function App() {
                     />
                   </ListItemIcon>
                   <ListItemText primary="% Male popluation 25 - 29" />
-                </ListItemButton>
-
-                <ListItemButton
-                  onClick={() => {
-                    setselectGlobes("worldPopulation");
-                  }}
-                >
-                  <ListItemIcon>
-                    <PublicIcon
-                      color={(() =>
-                        selectGlobes === "worldPopulation"
-                          ? "primary"
-                          : "inherit")()}
-                    />
-                  </ListItemIcon>
-                  <ListItemText primary="World Population" />
                 </ListItemButton>
               </List>
             </CardActions>
@@ -700,16 +614,6 @@ function App() {
               gutterBottom
             >
               Regular Map
-            </Typography>
-          );
-        case "choroplethGlobe":
-          return (
-            <Typography
-              sx={{ fontSize: 17, marginBottom: "30px", fontWeight: "bold" }}
-              color="text.secondary"
-              gutterBottom
-            >
-              Choropleth Globe
             </Typography>
           );
         case "incomeLevel":
